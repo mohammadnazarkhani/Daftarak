d7a10284cf3feabe600e43bbbdf38e9adfe6066d
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-18 00:15:09 +0330

    refactor: organized the view pager screens

---
+++ app/src/main/java/com/example/daftarak/ui/fragment/onboarding/screens/Screen4Fragment.java
@@ -0,0 +1,21 @@
+package com.example.daftarak.ui.fragment.onboarding.screens;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daftarak.R;
+
+public class Screen4Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_screen4, container, false);
+    }
+}
