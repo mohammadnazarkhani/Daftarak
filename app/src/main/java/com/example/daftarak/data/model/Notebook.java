a1e0e3fa2ebd8315fba88286ef448c1156d6c897
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-17 22:30:01 +0330

    feat: add Notebook and Note dao classes

---
+++ app/src/main/java/com/example/daftarak/data/model/Notebook.java
@@ -0,0 +1,54 @@
+package com.example.daftarak.data.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import org.jetbrains.annotations.NotNull;
+
+@Entity(tableName = "notebooks")
+public class Notebook {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @NotNull
+    @ColumnInfo(name = "created_at")
+    private long createdAt;
+
+    public Notebook(int id, @NonNull String title, @NotNull long createdAt) {
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+}
