8da82e3503daf1e587fe7c30d7a582ab5c4271fe
mohammadnazarkhani <122028480+mohammadnazarkhani@users.noreply.github.com>
2025-06-18 00:15:29 +0330

    feat: add ViewPagerAdapter

---
+++ app/src/main/java/com/example/daftarak/ui/fragment/onboarding/ViewPagerFragment.java
@@ -0,0 +1,27 @@
+package com.example.daftarak.ui.fragment.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daftarak.R;
+
+import java.util.ArrayList;
+
+public class ViewPagerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
+
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        return view;
+    }
+}
